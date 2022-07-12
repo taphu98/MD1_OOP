170552c1b17d1ecf8f9ceb574f2cdb38f6f19146
unknown <taphu0812@gmail.com>
2022-07-12 16:24:11 +0700

    update

---
+++ Cat.js
@@ -0,0 +1,22 @@
+class Cat extends Animal{
+
+    constructor(name, weight, speed, say) {
+        super(name, weight, speed, say);
+    }
+    catchRat(rat){
+        if(super.getSpeed()>rat.getSpeed()){
+            rat.setStatus(false);
+        }else {
+            rat.setStatus(true);
+        }
+    }
+    eatRat(rat) {
+        if (!rat.getStatus()){
+            console.log('khoi luong cua meo sau khi an thit chuot la ',super.getWeight()+rat.getWeight())
+        }else {
+            console.log('bat dc dau ma doi an')
+        }
+
+    }
+
+}
---
+++ Rat.js
@@ -0,0 +1,13 @@
+class Rat extends Animal{
+
+    constructor(name, weight, speed, say,status) {
+        super(name, weight, speed, say);
+        this.status = status;
+    }
+    getStatus(){
+        return this.status;
+    }
+    setStatus(status){
+        this.status = status;
+    }
+}
